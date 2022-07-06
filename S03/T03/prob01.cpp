6c799fad37616e177a78ce44080a37e123d53cc3
DangoMelon <gerardo_art@me.com>
2022-07-05 23:35:58 -0500

    Add S03 files

---
+++ S03/T03/prob01.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+int main()
+{
+    int number;
+    std::cout << "Ingrese un numero entero de 2 cifras: ";
+    std::cin >> number;
+
+    if (number < 10 || number > 99)
+    {
+        std::cout << "El numero ingresado no es de 2 cifras";
+    }
+    else
+    {
+
+        int first_digit = number / 10;
+        int second_digit = number % 10;
+
+        switch (first_digit)
+        {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 9:
+            std::cout << "El primer digito es primo" << std::endl;
+            break;
+        default:
+            std::cout << "El primer digito no es primo" << std::endl;
+            break;
+        }
+
+        switch (second_digit)
+        {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 9:
+            std::cout << "El segundo digito es primo" << std::endl;
+            break;
+        default:
+            std::cout << "El segundo digito no es primo" << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
