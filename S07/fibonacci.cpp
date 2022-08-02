4b08260cbe7621770f58a014280a4219584dc4e4
DangoMelon <gerardo_art@me.com>
2022-08-02 17:25:35 -0500

    Use a more general condition in the recursive function

--- S07/fibonacci.cpp
+++ S07/fibonacci.cpp
@@ -34,7 +34,7 @@ int fibonacci_iterativo(int n)
 
 int fibonacci_recursivo(int n)
 {
-    if (n == 1 || n == 2)
+    if (n < 3)
         return 1;
     else
         return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2);
